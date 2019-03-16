977a20345ffb932d6beae9f66ef45da2c767e88b
jiaruixu <jiaruixu@umich.edu>
2019-03-15 23:47:30 -0400

    add light2light

--- scripts/unit_light2light.sh
+++ scripts/unit_light2light.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+nvidia-docker run --rm -it \
+	-v /mnt/fcav/3D-lighting/dataset/light2light:/mnt/fcav/3D-lighting/dataset/light2light \
+	-v /mnt/fcav/3D-lighting/UNIT/results:/mnt/fcav/3D-lighting/UNIT/results \
+	-v `pwd`/:/root \
+	unit \
 python train.py \
     --config configs/unit_light2light.yaml \
     --trainer UNIT \
